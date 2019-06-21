e35bad454601f0038caaad4afa0f3a6e2a6f6dd4
davemc85 <davemcallister@hotmail.co.uk>
2019-06-21 17:06:25 +0100

    item.js set up

---
+++ src/components/item/Item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Item = ({item}) => {
+
+  if(!item){
+    return <p>item coming soon!...</p>
+  }
+
+  const url = "/items/" + item.id;
+
+  return (
+    <React.Fragment>
+      <Link to {url} className="item_name">
+        {item.name}
+      </Link>
+      <p>{item.description}</p>
+    </React.Fragment>
+  )
+}
+
+export default Item;
