eed5959778f56dacecaba7b4ed7be53b252e7a3c
sridevi <sdara20@gmail.com>
2019-06-25 11:49:53 +0100

    nothing changes done

--- src/containers/persons/PersonController.js
+++ src/containers/persons/PersonController.js
@@ -4,7 +4,7 @@ import PersonList from '../../components/person/PersonList';
 import PersonDetail from '../../components/person/PersonDetail';
 import Request from '../../helpers/Request';
 import PersonFormContainer from './PersonFormContainer';
-import PersonEditFormContainer from './PersonEditFormContainer';
+ import Person from '../../components/person/Person';
 
 
 class PersonController extends Component {
@@ -15,6 +15,7 @@ class PersonController extends Component {
     }
 
     this.findPersonById = this.findPersonById.bind(this);
+
     this.handlePost = this.handlePost.bind(this);
   }
 
@@ -31,19 +32,15 @@ class PersonController extends Component {
         return person.id === parseInt(id);
       })
     }
+
      handlePost(person){
     const request = new Request();
     request.post("/api/persons/", person)
     .then(() => {
       window.location = "/persons";
-    });
-  }
-  handlePersonUpdate(person, id){
-    const request = new Request();
-    request.patch('/api/persons/' + id, person).then(()=>{
-      window.location = '/persons/' + id;
     })
   }
+
 
   render(){
     return(
@@ -53,16 +50,10 @@ class PersonController extends Component {
           <Route exact path="/persons" render={() =>
             <PersonList persons ={this.state.persons} /> }/>
 
-            <Route exact path= '/persons/new' render={() =>{
+            <Route exact path= "/persons/new" render={() =>{
               return <PersonFormContainer handlePersonPost = {this.handlePost}/>
                 }}/>
 
-            <Route exact path='/persons/edit/:id' render={(props)=> {
-              const id = props.match.params.id
-              const person = this.findPersonById(id);
-              return <PersonEditFormContainer person={person}
-              handlePersonUpdate={this.handlePersonUpdate}/>
-            }}/>
             <Route exact path="/persons/:id"
               render={(props) => {
               const id = props.match.params.id;
@@ -74,7 +65,6 @@ class PersonController extends Component {
         </Router>
       )
     }
-  }
 
-
+}
   export default PersonController;
