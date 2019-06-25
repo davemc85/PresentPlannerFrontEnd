0cc390230cf82783a1e1c1a1ad3d17bfae242f49
davemc85 <davemcallister@hotmail.co.uk>
2019-06-25 11:24:18 +0100

    navbar tweaked

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,16 @@ import {Link} from 'react-router-dom';
 const NavBar = (props) => {
   return (
     <header>
-      <img src="https://wallpaperset.com/w/full/d/0/4/104135.jpg" alt="Logo"/>
       <h1>PRESENT PLANNER REMINDER</h1>
         <ul>
         <li className="navLink">
-          <a href="/persons">Persons</a>
-          </li>
-          <li className="navLink">
-            <a href="/dates">EventDates</a>
-            </li>
-            <li className="navLink">
-              <a href="/items">StockItems</a>
-              </li>
-              </ul>
-      </header>
+          <a href="/persons/new">Add a new person</a>
+        </li>
+        <li className="navLink">
+          <a href="/items/new">Add a new item</a>
+        </li>
+      </ul>
+    </header>
   )
 }
 
