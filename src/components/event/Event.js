c11cac3612cebfda0dbb5b3dfa73a571be78b7cb
annemck <annekh88@gmail.com>
2019-06-26 18:29:07 +0100

    days to event now shows in event detail component

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -7,10 +7,35 @@ const Event = ({event}) => {
   }
 
   const url = "/events/" + event.id;
+  
+  const getDays = () => {
+    if (!event.days){
+      let today = new Date();
+      let specialDay = new Date(event.eventDate);
+      if (specialDay.getYear() < today.getYear()){
+        specialDay.setFullYear(1201);
+        today.setFullYear(1200);
+      } else {
+        specialDay.setFullYear(1200);
+        today.setFullYear(1200);
+      }
+      if (specialDay.getDate() > today.getDate()){
+        const diffTime = Math.abs(specialDay.getTime() - today.getTime());
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays;
+      } else {
+        const diffTime = Math.abs(today.getTime() - specialDay.getTime());
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays;
+      }
+    } else {
+      return event.days
+    }
+  }
 
   return (
     <div>
-      <p> It is {event.person.name}'s {event.eventName} in <b>{event.days}</b> days</p>
+      <p> It is {event.person.name}'s {event.eventName} in <b>{getDays()}</b> days</p>
     </div>
   )
 
--- src/components/event/EventDetail.js
+++ src/components/event/EventDetail.js
@@ -10,15 +10,63 @@ const EventDetail = (props) => {
   const handleDeleteClick = () => {
     props.onDelete(props.event.id);
   }
+  
+  const getEventDate = () => {
+    const eventDate = new Date(props.event.eventDate);
+    const day = eventDate.getDate();
+    const month = eventDate.getMonth();
+    switch(month){
+      case 0:
+      return "January " + day
+      break;
+      case 1:
+      return "February " + day
+      break;
+      case 2:
+      return "March " + day
+      break;
+      case 3:
+      return "April " + day
+      break;
+      case 4:
+      return "May " + day
+      break;
+      case 5:
+      return "June " + day
+      break;
+      case 6:
+      return "July " + day
+      break;
+      case 7:
+      return "August " + day
+      break;
+      case 8:
+      return "September " + day
+      break;
+      case 9:
+      return "October " + day
+      break;
+      case 10:
+      return "November " + day
+      break;
+      case 11:
+      return "December " + day
+      break;
+      default:
+      return eventDate
+    }
+  }
+  
 
 
   return(
     <div className="item-component">
       <Event event={props.event}/>
-      <p>{props.eventDate}</p>
+      <p>on {getEventDate()}</p>
       <button onClick={handleDeleteClick}>Delete Event</button>
 
     </div>
   )
 }
+
 export default EventDetail;
